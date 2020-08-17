7882319e2567b49efc51ca276b3b8d0492bddaa1
jorgecif <jorgecif@gmail.com>
2020-08-17 10:56:15 -0500

    iris eda app deploy

---
+++ README.md
@@ -0,0 +1,9 @@
+#iris-eda-app
+
+Simple app streamlit
+
+# Required files
+1. setup.sh
+2. procfile
+3. requirements.txt
+
---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+
+    """A simple streamlit app"""
+
+    st.title("Iris EDA app with streamlit")
+    st.subheader("Streamlit is cool")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
